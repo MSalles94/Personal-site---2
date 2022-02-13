f181e61090193c5671b9ad50dcde5215e960d6d5
MSalles94 <matheus.salles_1994@hotmail.com>
2022-02-12 21:08:21 -0300

    upgrading site
    some modifications - trying to undestand github funtioning

---
+++ script.js
@@ -0,0 +1,77 @@
+//projects --
+
+var calculator = {nome:'calculator',detalhe:'First JS tutorial made.',imagem:'Assets/capa_calculator.png',link:'projects/calculator/calculator.html', grupo:'main'};
+
+var cronometro = {nome:'cronometro',detalhe:'Project copied from a JS tutorial and improved with some DOM manipulation.',imagem:'Assets/capa_cronometro.png',link:'projects/cronometer/cronometro_1.html',grupo:'main'};
+
+var parallax = {nome:'parallax',detalhe:'Project made to test a Front end tecnique. Not a big deal.',imagem:'Assets/capa_parallax.png',link:'projects/parallax/parallax.html',grupo:'other'};
+
+var landingPage = {nome:'landingPage', detalhe:'A web site to colect information from clients and make some kind of selling or email send. Simple but necessaire',imagem:'Assets/capa_landpg.png',link:'projects/landingpage/landingpage.html',grupo:'other'};
+
+//defining groups of projects 
+
+var projects = [calculator, cronometro,parallax,landingPage];
+
+var main = []
+var other = []
+
+var n = projects.length
+
+for(i=0 ; i<n ; i++){
+    if(projects[i].grupo=='main'){
+        main.push(projects[i])
+    }
+    else if(projects[i].grupo=='other'){
+        other.push(projects[i])
+    }
+}
+
+// print the projects onload HTML page
+function addBlock(objeto){
+
+    if (objeto.grupo=='main'){
+        local=".select_main_blocks";
+    } else if (objeto.grupo=='other'){
+        local=".select_other_blocks";
+    }
+    z1 = document.querySelector(local).innerHTML;
+    z2 = '<div onclick="load_proj('+objeto.nome+')" class="block"><img src="'+objeto.imagem+'"> <p>'+objeto.nome+'</p></div>'; 
+
+    document.querySelector(local).innerHTML = z1 + z2;
+}
+function loadBlock(){
+    for(i=0;i<n;i++){
+        addBlock(projects[i]);
+    }
+}
+
+// select and show the project
+
+function load_proj(objeto){
+    document.querySelector('.project img').setAttribute('src',objeto.imagem);
+
+    document.querySelector('.project a').setAttribute('href',objeto.link);
+
+    document.querySelector('.project_info_name').innerText = objeto.nome;
+
+    document.querySelector('.project_info_coments p').innerText = objeto.detalhe;
+
+}
+
+//change template
+    var template = true
+
+function change(){
+    if (template==true){
+        template = false;
+        document.querySelector('.template').setAttribute('href','style_template2.css');        
+    } else { template = true
+        document.querySelector('.template').setAttribute('href','style_template.css');
+    }
+
+
+
+}
+
+
+
